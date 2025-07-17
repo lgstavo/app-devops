dbee80fe9fd9b47756979cc7f304c7c3dcdc8df7
Luiz Gustavo <91857747+lgstavo@users.noreply.github.com>
2025-07-17 09:41:22 -0300

    Update UserRepository.java

--- backend/src/main/java/br/ufscar/dc/dsw/backend/repository/UserRepository.java
+++ backend/src/main/java/br/ufscar/dc/dsw/backend/repository/UserRepository.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // Este método você já tinha, para o login
     Optional<User> findByUsername(String username);
 
     @Query(
